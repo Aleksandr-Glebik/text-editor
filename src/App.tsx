5393643528efe87c858f3c4d1483b7ce7a56dec3
Aleksandr-Glebik <glebikaa@mail.ru>
2022-12-04 22:37:02 +0300

    feat: add input ul and li

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,70 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 import './App.scss'
 
+// type DisabledType = 'disabled' | '' | undefined
+type DisabledType = false | true | undefined
+
+interface NoteType {
+  id: string | undefined
+  text: string | undefined
+  disabled: DisabledType
+}
+
 function App() {
+  const [text, setText] = useState<string>('')
+  const [notes, setNotes] = useState<NoteType[]>([])
+
+  // const [isDisabled, setIsDisabled] = useState<DisabledType>(false);
+
+  const addNote = () => {
+    if (text.trim().length) {
+      setNotes([
+        ...notes,
+        {
+          id: new Date().toISOString(),
+          text,
+          disabled: true
+        }
+      ])
+      setText('')
+    }
+  }
+
+  const removeNote = (noteId: string | undefined) => {
+    setNotes(notes.filter(note => note.id !== noteId))
+  }
+
+  const changeNote = (noteId: string | undefined) => {
+    setNotes(notes.map( note => {
+        if (note.id !== noteId) {
+          return note
+        } else if (note.id == noteId) {
+          return {
+            ...note,
+            disabled: false
+          }
+        }
+      }) as NoteType[]
+    )
+  }
+
   return (
     <div className="App">
-      start project
+      <label>
+        <input placeholder='new note' value={text} onChange={e => setText(e.target.value)} />
+        <button onClick={addNote}>add note</button>
+      </label>
+      <ul>
+        {
+          notes.map( note => (
+            <li key={note.id}>
+              <input placeholder='note' defaultValue={note.text} disabled={true}/>
+              <button onClick={() => changeNote(note.id)}>change</button>
+              <button onClick={() => removeNote(note.id)}>delete</button>
+            </li>
+          ))
+        }
+      </ul>
     </div>
   )
 }
