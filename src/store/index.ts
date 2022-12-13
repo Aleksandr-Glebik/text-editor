1e34a7c6ec208ffcd54867c4173e254f0d3db91e
Aleksandr-Glebik <glebikaa@mail.ru>
2022-12-13 13:13:04 +0300

    feat: add subscriber on store, add func save, load from LocalStorage state

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,14 +2,39 @@ import { configureStore } from "@reduxjs/toolkit";
 import noteReducer from './noteSlice'
 import tagReducer from './tagSlice'
 
+const saveToLocalStorage = (state: any) => {
+    try {
+        const initialState = JSON.stringify(state)
+        localStorage.setItem('state', initialState)
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+const loadFromLocalStorage = () => {
+    try {
+        const stateStr = localStorage.getItem('state')
+        return stateStr ? JSON.parse(stateStr) : undefined
+    } catch (e) {
+        console.error(e)
+        return undefined
+    }
+}
+
 const store = configureStore({
     reducer: {
         notes: noteReducer,
         tags: tagReducer,
-    }
+    },
+    preloadedState: loadFromLocalStorage()
 })
 
 export default store
 
+store.subscribe( () => {
+    saveToLocalStorage(store.getState())
+})
+
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
+
